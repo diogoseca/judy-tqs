2525d1de2bf1feb08bbb31e4e127ebaee04fa222
Jorge Ferreira <up201305617@fe.up.pt>
2020-12-19 20:42:29 +0000

    Update StudentTest.java

--- Cli-32f/src/test/java/org/apache/commons/cli/StudentTest.java
+++ Cli-32f/src/test/java/org/apache/commons/cli/StudentTest.java
@@ -7,15 +7,15 @@ import java.lang.NullPointerException;
 
 public class StudentTest extends TestCase
 {
-     public void testAIR_Div_910 (){
+	public void testAIR_Div_910 (){
 		HelpFormatter formatter = new HelpFormatter();
 		int result = formatter.findWrapPos("hello\n",  5, 0);
 		assertEquals(6, result);
 	}
 	
 	public void testFindWrapPos1(){
-        HelpFormatter formatter = new HelpFormatter();
-        assertEquals(formatter.findWrapPos("\ta\r\t", 2, 1), 2);
+		HelpFormatter formatter = new HelpFormatter();
+		assertEquals(formatter.findWrapPos("\ta\r\t", 2, 1), 2);
 		assertEquals(formatter.findWrapPos("\n1\n233", 2, 3), 5);
 		assertEquals(formatter.findWrapPos("A\n\rB", 10, -3), 2);
 		assertEquals(formatter.findWrapPos("ABC", 2, 1), -1);
@@ -24,8 +24,8 @@ public class StudentTest extends TestCase
 		assertEquals(formatter.findWrapPos("\ta\r\t", 4, 4), -1);
 		assertEquals(formatter.findWrapPos("\ta\r\t", 1, 2), 3);
 		 try {
-            formatter.findWrapPos("AB\tD", 0, -3);
-            fail();
-        } catch (java.lang.StringIndexOutOfBoundsException ex){}
-    }
+			formatter.findWrapPos("AB\tD", 0, -3);
+			fail();
+		} catch (java.lang.StringIndexOutOfBoundsException ex){}
+	}
 } //end of testclass
